e90aff8f729af4e456faedfb643232d5595ef8da
AnchorConcept <animashaunolajide2@gmail.com>
2023-09-08 19:10:31 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - prints "Programming is like a building a multilingual puzzle, followed by new line.
+ * main - prints "Programming is like a building a multilingual puzzle
  * Return: Always 0.
  */
 
 int main(void)
-{ puts("\"Programming is like building a multilingual puzzle, followed by a new line.");
+{ puts("\"Programming is like building a multilingual puzzle");
 		return (0);
 	}
