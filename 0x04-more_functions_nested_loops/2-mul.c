780743b8098ebe7692f959216d1cb1ef9b9be0be
AnchorConcept <animashaunolajide2@gmail.com>
2023-09-14 23:17:34 -0700

    EMPTY UPDATED

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+* __mul - multiply 2 int
+* @a: the first int
+* @b: the second int
+*Return: addition of the multiplication
+*/
+int mul(int a, int b)
+{
+return (a * b)
+}
